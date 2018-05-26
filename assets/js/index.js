e5bd3c3ff0b97d7917e300b1316299e0215baa07
yuanlin1231 <278023077@qq.com>
2018-05-26 18:06:08 +0800

    首页

---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+  $scope.records = [
+    {
+      "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
+      "title" : "Alfreds Futterkiste",
+      "teachername" : "Germany",
+      "value":"免费"
+    },
+    {
+        "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
+        "title" : "Alfreds Futterkiste",
+        "teachername" : "Germany",
+        "value":"免费"
+    },
+    {
+        "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
+        "title" : "Alfreds Futterkiste",
+        "teachername" : "Germany",
+        "value":"免费"
+    },
+    {
+        "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
+        "title" : "Alfreds Futterkiste",
+        "teachername" : "Germany",
+        "value":"免费"
+    },
+    {
+        "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
+        "title" : "Alfreds Futterkiste",
+        "teachername" : "Germany",
+        "value":"免费"
+    },
+    {
+        "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
+        "title" : "Alfreds Futterkiste",
+        "teachername" : "Germany",
+        "value":"免费"
+    },
+    {
+        "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
+        "title" : "Alfreds Futterkiste",
+        "teachername" : "Germany",
+        "value":"免费"
+    },
+    {
+        "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
+        "title" : "Alfreds Futterkiste",
+        "teachername" : "Germany",
+        "value":"免费"
+    }
+    
+  ]
+});
