ce7c9f095b46620f9daae4d55431a7c98a1586c2
Hylance1224 <1084305363@qq.com>
2018-06-09 11:35:45 +0800

    学习“

---
+++ components/study_detail/study_detail.js
@@ -0,0 +1,7 @@
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+    $scope.title="人工智能";
+    $scope.teacher="赵老师"
+    $scope.img="http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg"
+
+});
