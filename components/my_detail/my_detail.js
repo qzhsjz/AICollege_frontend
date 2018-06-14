51ef8091386420f148d14e8dab0e7487c9f304d8
yuanlin1231 <278023077@qq.com>
2018-06-14 22:38:17 +0800

    增加邀请码

--- components/my_detail/my_detail.js
+++ components/my_detail/my_detail.js
@@ -4,9 +4,12 @@ $(function(){
     var email = "YDS";
     var QQ = "YDS";
     var weixin = "YDS";
+    var UserID = "122345";
     document.getElementById("nickname").setAttribute("placeholder",nickname);
     document.getElementById("email").setAttribute("placeholder",email);
     document.getElementById("QQ").setAttribute("placeholder",QQ);
     document.getElementById("weixin").setAttribute("placeholder",weixin);
+    document.getElementById("UserID").innerHTML = UserID;
+    
     
  })
