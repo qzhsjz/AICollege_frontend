bc8abf696936985832dd31dd3779bc71d3672bf1
yuanlin1231 <278023077@qq.com>
2018-06-18 15:31:29 +0800

    tab

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,176 +1,16 @@
 var urlcookie = 'http://39.106.19.27:8080/user/getuserinfo';
-// $.ajax({
-//     url:urlcookie,
-//     type:'get',
-//     data: {
-        
-//     },
-//     xhrFields: {
-//         withCredentials: true
-//     },
-//     crossDomain: true,
-//     dataType: 'json',
-//     success: function(data) {
-//         if(data.error){
-            
-//           }else{
-            
 
-//           }
-
-
-         
-      
-//     }
-// });
-    
-    
 var urlget = 'http://39.106.19.27:8080/course/all/';
 var hotcourse;
 var hotpage ;
 var newpage ;
-//  var hotpagelen;
 
 
  var newcourse;
-//    = [
-//     {
-//       "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
-//       "title" : "Alfreds Futterkiste",
-//       "teachername" : "Germany",
-//       "value":"免费2"
-//     },
-//     {
-//         "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
-//         "title" : "Alfreds Futterkiste",
-//         "teachername" : "Germany",
-//         "value":"免费"
-//     },
-//     {
-//         "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
-//         "title" : "Alfreds Futterkiste",
-//         "teachername" : "Germany",
-//         "value":"免费"
-//     },
-//     {
-//         "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
-//         "title" : "Alfreds Futterkiste",
-//         "teachername" : "Germany",
-//         "value":"免费"
-//     },
-//     {
-//         "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
-//         "title" : "Alfreds Futterkiste",
-//         "teachername" : "Germany",
-//         "value":"免费"
-//     },
-//     {
-//         "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
-//         "title" : "Alfreds Futterkiste",
-//         "teachername" : "Germany",
-//         "value":"免费"
-//     },
-//     {
-//         "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
-//         "title" : "Alfreds Futterkiste",
-//         "teachername" : "Germany",
-//         "value":"免费"
-//     },
-//     {
-//         "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
-//         "title" : "Alfreds Futterkiste",
-//         "teachername" : "Germany",
-//         "value":"免费"
-//     }
-   
-//   ];
 
-//   var hotpage = [1,2,3];
-//   var newpage = [1,2];
  
  var course = angular.module('Course', []);
-//   course.controller("course", function($scope) {
-//     //   alert(111);
 
-
-//     $.ajax({
-//         url:urlget,
-//         type:'post',
-//         data: {
-         
-//         },
-//         dataType: 'json',
-//         success: function(data) {
-//          //  console.log(JSON.stringify(data));
-//           if(data.error){
-//            alert(data.error);
-//          }else{
-         
-//              hotcourse = data;
-            
-//             //  hotpagelen = parseInt(data.length/9)+1;
-          
-//              // for(i=0;i<hotpagelen;i++){
-//              //     hotpage[i]=i+1;
-//              // }
-//              // hotpage[0] = 1;
-//             //  var len = 9;
-//              hotpage = new Array(data.length);
-//              console.log(hotpage);
-//              for(i=0;i<len;i++){
-//                  hotpage[i]=i+1;
-//              }
-//              console.log(JSON.stringify(hotcourse));
-//              $scope.hot = hotcourse;
-//              $scope.new = newcourse;
-//              $scope.hotpage = hotpage;
-//              $scope.newpage = newpage;
-//              console.log(JSON.stringify($scope.hot));
-//             //  console.log(JSON.stringify(hotcourse[0].teacherName));
-            
-//              $scope.hotpagechange = function(event) {
-//                  var page = $(event.target).html();
-//                      hotcourse = [
-//                          {
-//                            "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
-//                            "title" : "Alfreds Futterkiste",
-//                            "teachername" : "Germany",
-//                            "value":"免费3"
-//                          }
-                        
-//                        ];
-//                        $scope.hot = hotcourse;
-//                  // alert(page);
-//              };
-//              $scope.newpagechange = function(event) {
-//                  var page = $(event.target).html();
-//                      newcourse = [
-//                          {
-//                            "img":"http://7zn2fy.com1.z0.glb.clouddn.com/apicloud/ffc1a309e40d8272b5bf0975170762c7.jpg",
-//                            "title" : "Alfreds Futterkiste",
-//                            "teachername" : "Germany",
-//                            "value":"免费3"
-//                          }
-                        
-//                        ];
-//                        $scope.new = newcourse;
-//                  // alert(page);
-//              };
-            
-//              $scope.chooseItem=function(index){
-//                  var ch=$scope.new[index];
-//                  var name=ch.teachername;
-//                  //location.href = "components/course_buy/course_buy.html?&id=" + name;
-//                  window.open("components/course_buy/course_buy.html?&id=" + name);
-//              }
-//          }
-//         }
-//      });
-
-
-  
-   
-// });
 
 var quit = function(){
     var urlquit = 'http://39.106.19.27:8080/user/logout';
@@ -187,7 +27,7 @@ var quit = function(){
           if(data.error){
            alert(data.error);
          }else{
-          location.href = "../../index.html";
+          location.href = "index.html";
          }
         }
     });
@@ -215,6 +55,8 @@ course.controller('course', ['$scope', '$http', function ($scope, $http) {
                 $('.head').on('mouseleave',function(){
                     $('.bubble').css("display","none");
                 });
+                $('.headimg').href = "../../components/my_detail/my_detail.html";
+                $('.headimg').target="_blank"
             }else{
                 $(".mystudy").hide();
             }
