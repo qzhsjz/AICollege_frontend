cd2bbc30f2f9e9f0274b4673c887dca936b45a19
baixuefeng <505125785@qq.com>
2018-09-04 23:03:47 +0800

    lsls

--- components/news/news.js
+++ components/news/news.js
@@ -1,5 +1,8 @@
 var urlcookie = 'http://39.106.19.27:8080/user/getuserinfo';
+var urlnew =  'http://39.106.19.27:8080/message/getmsg';
 var app = angular.module('myApp', []);
+
+
 app.controller('personCtrl',['$scope', '$http', function($scope, $http, $location) {
     $http({
         method: 'get', //get请求方式
@@ -26,31 +29,83 @@ app.controller('personCtrl',['$scope', '$http', function($scope, $http, $locatio
                 $(".mystudy").hide();
             }
         }
-        $scope.box1 = true;
-    $scope.toggle1 = function() {
-        $('.list1').addClass('active');
-        $('.list2').removeClass('active');
-        $('.list3').removeClass('active');
-        $('.Classinf').removeClass('dis');
-        $('.Webinf').addClass('dis');
-        $('.Interactioninf').addClass('dis');
-    };
-    $scope.toggle2 = function() {
-        $('.list1').removeClass('active');
-        $('.list2').addClass('active');
-        $('.list3').removeClass('active');
-        $('.Classinf').addClass('dis');
-        $('.Webinf').removeClass('dis');
-        $('.Interactioninf').addClass('dis');
-    };
-    $scope.toggle3 = function() {
-        $('.list1').removeClass('active');
-        $('.list2').removeClass('active');
-        $('.list3').addClass('active');
-        $('.Classinf').addClass('dis');
-        $('.Webinf').addClass('dis');
-        $('.Interactioninf').removeClass('dis');
-    };
+        $http({
+            method: 'get', //get请求方式
+            url: urlnew,   //请求地址
+            withCredentials: true,
+        }).then(function (response) {
+            console.log(JSON.stringify(response.data.Message));
+            $scope.box1 = true;
+            $scope.Message = response.data.Message;
+            $scope.Announcement = response.data.Announcement;
+            if(response.data.Announcement.length==0){
+                $('.Webinf').removeClass('dis');
+                $scope.toggle1 = function() {
+                    $('.list2').removeClass('active');
+                    $('.list1').addClass('active');
+                    $('.Classinf').addClass('dis');
+                    $('.Webinf').removeClass('dis');
+                    $('.Interactioninf').addClass('dis');
+                };
+            }else{
+                $scope.toggle1 = function() {
+                    console.log(2);
+                    $('.list1').addClass('active');
+                    $('.list2').removeClass('active');
+                    $('.Classinf').removeClass('dis');
+                    $('.Webinf').addClass('dis');
+                    $('.Interactioninf').addClass('dis');
+                };
+            }
+            if(response.data.Message.length==0){
+                $scope.toggle2 = function() {
+                    console.log(1);
+                    $('.list1').removeClass('active');
+                    $('.list2').addClass('active');
+                    $('.Classinf').addClass('dis');
+                    $('.Webinf').removeClass('dis');
+                    $('.Interactioninf').addClass('dis');
+                };
+            }else{
+                $scope.toggle2 = function() {
+                    $('.list1').removeClass('active');
+                    $('.list2').addClass('active');
+                    $('.Classinf').addClass('dis');
+                    $('.Webinf').addClass('dis');
+                    $('.Interactioninf').removeClass('dis');
+                };
+            }
+            // $scope.toggle1 = function() {
+            //     $('.list1').addClass('active');
+            //     $('.list2').removeClass('active');
+            //     $('.list3').removeClass('active');
+            //     $('.Classinf').removeClass('dis');
+            //     $('.Webinf').addClass('dis');
+            //     $('.Interactioninf').addClass('dis');
+            // };
+            // $scope.toggle2 = function() {
+            //     $('.list1').removeClass('active');
+            //     $('.list2').addClass('active');
+            //     $('.list3').removeClass('active');
+            //     $('.Classinf').addClass('dis');
+            //     $('.Webinf').removeClass('dis');
+            //     $('.Interactioninf').addClass('dis');
+            // };
+            // $scope.toggle3 = function() {
+            //     $('.list1').removeClass('active');
+            //     $('.list2').removeClass('active');
+            //     $('.list3').addClass('active');
+            //     $('.Classinf').addClass('dis');
+            //     $('.Webinf').addClass('dis');
+            //     $('.Interactioninf').removeClass('dis');
+            // };
+        }, function (response) {
+            //失败时执行 
+            console.log("CookieError" + response);
+            alert("88881");
+        
+        });//请求用户id
+        
 }, function (response) {
     //失败时执行 
     console.log("CookieError" + response);
