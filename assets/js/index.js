045444c097272ca3229bd23e93b3915f10ac050f
baixuefeng <505125785@qq.com>
2018-09-06 21:06:33 +0800

    lala

--- assets/js/index.js
+++ assets/js/index.js
@@ -55,6 +55,13 @@ $('.find_button').on('click',function(){
     //     }
     // });
 });
+function onKeyDown(event){
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+        if(e && e.keyCode==13){ // enter 键
+            var search_text = $('#search_area').val();
+            location.href = "search.html?key="+search_text;
+        }    
+}
 
 course.controller('course', ['$scope', '$http', function ($scope, $http) {
     
